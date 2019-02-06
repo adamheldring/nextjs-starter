5a3a0ccad61dde876eff3565811f2ade41d3b035
Faisal Mahmud <faisal@willandskill.se>
2019-02-06 11:50:56 +0100

    Add Collapse

--- sections/news/components/NewsListItem.jsx
+++ sections/news/components/NewsListItem.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 
+import { Collapse } from "reactstrap";
 import TimeAgo from "react-timeago";
 
-const NewsListItem = props => {
-  const { story } = props;
-  return (
-    <div className="row">
-      <div className="col-12">
-        <div className="card mb-2">
-          {story.image && (
-            <img className="img-fluid" src={story.image} alt={story.heading} />
-          )}
-          <div className="card-body p-3">
-            <p className="card-text mb-1">{story.heading}</p>
-            <p className="text-muted">
-              <small>
-                {story.provider.name} <strong>&middot;</strong>{" "}
-                <TimeAgo date={story.published_at} />
-              </small>
-            </p>
+class NewsListItem extends Component {
+  state = { collapse: false };
+
+  toggleCollapse = () => {
+    this.setState({ collapse: !this.state.collapse });
+  };
+
+  render() {
+    const { story } = this.props;
+    return (
+      <div className="row" onClick={this.toggleCollapse}>
+        <div className="col-12">
+          <div className="card mb-2">
+            {story.image && (
+              <img
+                className="img-fluid"
+                src={story.image}
+                alt={story.heading}
+              />
+            )}
+            <div className="card-body p-3">
+              <h6 className="card-text mb-1">{story.heading}</h6>
+              <Collapse isOpen={this.state.collapse}>
+                <p className="mb-2">{story.summary || story.body}</p>
+              </Collapse>
+              <p className="text-muted">
+                <small>
+                  {story.provider.name} <strong>&middot;</strong>{" "}
+                  <TimeAgo date={story.published_at} />
+                </small>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default NewsListItem;
